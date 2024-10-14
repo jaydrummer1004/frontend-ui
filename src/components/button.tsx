2714f87a6c0e4a2dfc2db268a5df106764a6c0c9
Justin Landis <justilandis1@gmail.com>
2024-10-13 20:44:52 -0500

    style buttons

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -7,17 +7,22 @@ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof buttonVariants>;
 
 const buttonVariants = cva(
-  'inline-flex items-center rounded justify-center font-medium [&>[data-icon]]:size-[1em] [&>[data-icon]]:fill-current focus-visible:outline-none',
+  'inline-flex items-center rounded justify-center font-medium [&>[data-icon]]:fill-current focus-visible:outline-none',
   {
     variants: {
       variant: {
-        primary: 'bg-indigo-700 hover:bg-indigo-800 text-white focus:bg-indigo-800 active:bg-indigo-800',
+        primary: 'bg-indigo-700 hover:bg-indigo-800 text-white focus:bg-indigo-800 active:bg-indigo-800 shadow-sm',
+        secondary: 'bg-white border-[0.5px] border-solid border-neutral-200 text-neutral-900 shadow-sm',
+        tertiary: 'bg-white text-indigo-700',
+        destructive: 'bg-red-600 text-white',
+        linkPrimary: 'text-indigo-700',
+        linkSecondary: 'text-neutral-900'
       },
       size: {
-        md: 'text-sm gap-1 px-3.5 py-2.5',
-        lg: 'text-base gap-1.5 px-4 py-2',
-        xl: 'text-base gap-1.5 px-5 py-3',
-        xxl: 'text-lg gap-2.5 px-6 py-4',
+        md: 'text-sm gap-1 px-3.5 py-2.5 [&>[data-icon]]:size-4',
+        lg: 'text-base gap-1.5 px-4 py-2.5 [&>[data-icon]]:size-4',
+        xl: 'text-base gap-1.5 px-5 py-3 [&>[data-icon]]:size-4',
+        xxl: 'text-lg gap-2.5 px-6 py-4 [&>[data-icon]]:size-5',
       },
       iconOnly: {
         true: 'gap-2',
@@ -26,25 +31,48 @@ const buttonVariants = cva(
     },
     compoundVariants: [
       {
+        variant: ['linkSecondary', 'linkPrimary'],
+        className: 'p-0',
+      },
+      {
+        variant: ['primary', 'secondary', 'tertiary', 'destructive'],
+        size: 'lg',
+        className: 'px-4 py-2.5',
+      },
+      {
+        variant: ['primary', 'secondary', 'tertiary', 'destructive'],
+        size: 'xl',
+        className: 'px-5 py-3',
+      },
+      {
+        variant: ['primary', 'secondary', 'tertiary', 'destructive'],
+        size: 'xxl',
+        className: 'px-6 py-4',
+      },
+      {
+        variant: ['primary', 'secondary', 'tertiary', 'destructive', 'linkPrimary', 'linkSecondary'],
         iconOnly: true,
         size: 'md',
-        className: 'p-2.5'
+        className: 'p-2.5 gap-2 ',
       },
       {
+        variant: ['primary', 'secondary', 'tertiary', 'destructive', 'linkPrimary', 'linkSecondary'],
         iconOnly: true,
         size: 'lg',
-        className: 'p-3'
+        className: 'p-3 gap-2',
       },
       {
+        variant: ['primary', 'secondary', 'tertiary', 'destructive', 'linkPrimary', 'linkSecondary'],
         iconOnly: true,
-        size: 'lg',
-        className: 'p-3.5'
+        size: 'xl',
+        className: 'p-3.5',
       },
       {
+        variant: ['primary', 'secondary', 'tertiary', 'destructive', 'linkPrimary', 'linkSecondary'],
         iconOnly: true,
-        size: 'lg',
-        className: 'p-4'
-      }
+        size: 'xxl',
+        className: 'p-4 gap-2',
+      },
     ],
     defaultVariants: {
       variant: 'primary',
--- src/examples/button-example.tsx
+++ src/examples/button-example.tsx
@@ -3,6 +3,7 @@ import { Star } from '../icons/star';
 
 export function ButtonExample() {
   return (
+    <div className='grid gap-12'>
       <div className="flex flex-wrap items-center gap-5">
         <Button size="md">Button CTA</Button>
         <Button size="lg">
@@ -14,7 +15,85 @@ export function ButtonExample() {
           <Star />
         </Button>
         <Button size="xxl">Button CTA</Button>
-        <Button><Star /></Button>
+        <Button size="xxl" iconOnly>
+          <Star />
+        </Button>
       </div>
+      <div className="flex flex-wrap items-center gap-5">
+        <Button variant="secondary" size="md">Button CTA</Button>
+        <Button variant="secondary" size="lg">
+          <Star />
+          Button CTA
+        </Button>
+        <Button variant="secondary" size="xl">
+          Button CTA
+          <Star />
+        </Button>
+        <Button variant="secondary" size="xxl">Button CTA</Button>
+        <Button variant="secondary" size="xxl" iconOnly>
+          <Star />
+        </Button>
+      </div>
+      <div className="flex flex-wrap items-center gap-5">
+        <Button variant="tertiary" size="md">Button CTA</Button>
+        <Button variant="tertiary" size="lg">
+          <Star />
+          Button CTA
+        </Button>
+        <Button variant="tertiary" size="xl">
+          Button CTA
+          <Star />
+        </Button>
+        <Button variant="tertiary" size="xxl">Button CTA</Button>
+        <Button variant="tertiary" size="xxl" iconOnly>
+          <Star />
+        </Button>
+      </div>
+      <div className="flex flex-wrap items-center gap-5">
+        <Button variant="linkPrimary" size="md">Button CTA</Button>
+        <Button variant="linkPrimary" size="lg">
+          <Star />
+          Button CTA
+        </Button>
+        <Button variant="linkPrimary" size="xl">
+          Button CTA
+          <Star />
+        </Button>
+        <Button variant="linkPrimary" size="xxl">Button CTA</Button>
+        <Button variant="linkPrimary" size="xxl" iconOnly>
+          <Star />
+        </Button>
+      </div>
+      <div className="flex flex-wrap items-center gap-5">
+        <Button variant="linkSecondary" size="md">Button CTA</Button>
+        <Button variant="linkSecondary" size="lg">
+          <Star />
+          Button CTA
+        </Button>
+        <Button variant="linkSecondary" size="xl">
+          Button CTA
+          <Star />
+        </Button>
+        <Button variant="linkSecondary" size="xxl">Button CTA</Button>
+        <Button variant="linkSecondary" size="xxl" iconOnly>
+          <Star />
+        </Button>
+      </div>
+      <div className="flex flex-wrap items-center gap-5">
+        <Button variant="destructive" size="md">Button CTA</Button>
+        <Button variant="destructive" size="lg">
+          <Star />
+          Button CTA
+        </Button>
+        <Button variant="destructive" size="xl">
+          Button CTA
+          <Star />
+        </Button>
+        <Button variant="destructive" size="xxl">Button CTA</Button>
+        <Button variant="destructive" size="xxl" iconOnly>
+          <Star />
+        </Button>
+      </div>
+    </div>
   );
 }
