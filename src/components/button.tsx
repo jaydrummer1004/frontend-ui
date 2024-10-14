037c4fa62b32b101b15770268f2f9465a8de67e0
Justin Landis <justinlandis1@gmail.com>
2024-10-13 19:24:05 -0500

    add more stuff to button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,31 +6,61 @@ import { cn } from '../utils/cn';
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof buttonVariants>;
 
-const buttonVariants = cva('inline-flex items-center justify-center text-white py-2 [&>[data-icon]]:h-4 [&>[data-icon]]:fill-current', {
-  variants: {
-    variant: {
-      primary: 'bg-indigo-700 rounded',
+const buttonVariants = cva(
+  'inline-flex items-center rounded justify-center font-medium [&>[data-icon]]:size-[1em] [&>[data-icon]]:fill-current focus-visible:outline-none',
+  {
+    variants: {
+      variant: {
+        primary: 'bg-indigo-700 hover:bg-indigo-800 text-white focus:bg-indigo-800 active:bg-indigo-800',
+      },
+      size: {
+        md: 'text-sm gap-1 px-3.5 py-2.5',
+        lg: 'text-base gap-1.5 px-4 py-2',
+        xl: 'text-base gap-1.5 px-5 py-3',
+        xxl: 'text-lg gap-2.5 px-6 py-4',
+      },
+      iconOnly: {
+        true: 'gap-2',
+        false: '',
+      },
     },
-    size: {
-      sm: 'text-sm px-3.5 py-2.5',
-      md: 'text-md gap-2 px-4 py-2.5',
-      lg: 'text-lg px-5 py-2.5',
+    compoundVariants: [
+      {
+        iconOnly: true,
+        size: 'md',
+        className: 'p-2.5'
+      },
+      {
+        iconOnly: true,
+        size: 'lg',
+        className: 'p-3'
+      },
+      {
+        iconOnly: true,
+        size: 'lg',
+        className: 'p-3.5'
+      },
+      {
+        iconOnly: true,
+        size: 'lg',
+        className: 'p-4'
+      }
+    ],
+    defaultVariants: {
+      variant: 'primary',
+      size: 'md',
     },
   },
-  defaultVariants: {
-    variant: 'primary',
-    size: 'md',
-  },
-});
+);
 
 function _Button(props: ButtonProps, ref: Ref<HTMLButtonElement>) {
-  const { className, size, variant, children, disabled } = props;
+  const { className, size, iconOnly, variant, children, disabled } = props;
 
   return (
     <button
       ref={ref}
       disabled={disabled}
-      className={cn(buttonVariants({ variant, size }), className)}
+      className={cn(buttonVariants({ variant, size, iconOnly }), className)}
     >
       {children}
     </button>
--- src/examples/button-example.tsx
+++ src/examples/button-example.tsx
@@ -3,10 +3,18 @@ import { Star } from '../icons/star';
 
 export function ButtonExample() {
   return (
-    <div className='grid gap-2'>
-      <Button size="sm">Button CTA</Button>
-      <Button size="md"><Star />Button CTA</Button>
-      <Button size="lg">Button CTA</Button>
-    </div>
+      <div className="flex flex-wrap items-center gap-5">
+        <Button size="md">Button CTA</Button>
+        <Button size="lg">
+          <Star />
+          Button CTA
+        </Button>
+        <Button size="xl">
+          Button CTA
+          <Star />
+        </Button>
+        <Button size="xxl">Button CTA</Button>
+        <Button><Star /></Button>
+      </div>
   );
 }
